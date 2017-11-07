3211c16243405c18547b94ad42793c149a9ad861
Maulik Patel <patelmr3@hotmail.com>
2017-11-07 11:07:21 -0500

    8

--- uzer/src/app/main-nav/main-nav.component.ts
+++ uzer/src/app/main-nav/main-nav.component.ts
@@ -1,10 +1,26 @@
 import { UsersService } from './../users/users.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  styleUrls: ['./main-nav.component.scss'],
+  animations: [
+    trigger('userDropdownTargetAnimate', [
+      state('void', style({
+        display: 'none',
+        opacity: 0,
+        transform: 'translateY(-20px)'
+      })),
+      state('true', style({
+        display: 'block',
+        opacity: 1,
+        transform: 'translateY(0px)'
+      })),
+      transition('void <=> true', animate('.2s ease-in-out'))
+    ])
+  ]
 })
 export class MainNavComponent implements OnInit {
   numUsers = 0;
